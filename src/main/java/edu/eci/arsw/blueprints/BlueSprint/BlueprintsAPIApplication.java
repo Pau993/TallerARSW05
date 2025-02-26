0a59edee078ffc54eb4e7818141c12e839269ab3
Paulinguis993 <99148906+Pau993@users.noreply.github.com>
2025-02-26 01:05:11 -0500

    Primer y segundo punto

--- src/main/java/edu/eci/arsw/blueprints/BlueSprint/BlueprintsAPIApplication.java
+++ src/main/java/edu/eci/arsw/blueprints/BlueSprint/BlueprintsAPIApplication.java
@@ -1,11 +1,9 @@
-package edu.eci.arsw.blueprintsapi;
+package edu.eci.arsw.blueprints.BlueSprint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"edu.eci.arsw.blueprints"})
+@SpringBootApplication(scanBasePackages = "edu.eci.arsw.blueprints")
 public class BlueprintsAPIApplication {
 
 	public static void main(String[] args) {
